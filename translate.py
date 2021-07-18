3bd3052ee05db6fb276ed1a043d01032c5cac271
Yura <87028237+Odinson137@users.noreply.github.com>
2021-07-18 19:50:16 +0300

    Update translate.py

--- translate.py
+++ translate.py
@@ -3,11 +3,12 @@ from deep_translator import GoogleTranslator
 
 def translated_buffer(langueage='ru', text='HELLO'):
     print(f"History:")
+    copy_text = pyperclip.paste()
         
     while True:
         translated = GoogleTranslator(source='auto', target=langueage).translate(text)
-        if translated != pyperclip.paste():
-            text = pyperclip.paste()
+        if translated != copy_text:
+            text = copy_text
             translated = GoogleTranslator(source='auto', target=langueage).translate(text)
             pyperclip.copy(translated)
             print(f"{text} - {translated}")
